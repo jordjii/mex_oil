24a9f1efda79dda1ce7c9f4a27ea091a4736e9a0
jordjii <georgiy.alexandrov@bk.ru>
2024-04-14 18:56:47 +0300

    v 0.1

--- mex_oil/settings.py
+++ mex_oil/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mex_oil.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / './templates/site']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- oil/urls.py
+++ oil/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('mexoil', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('profile', views.profile, name='profile'),
 ]
